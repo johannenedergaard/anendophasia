c08d9b9e44a630a33d2ee9b9ae0aa17e6ea708aa
johannenedergaard <nedergaard@cc.au.dk>
2023-07-08 11:06:12 +0200

    anonymized + supplemental fixed

--- anendophasia_correlation_plots.R
+++ anendophasia_correlation_plots.R
@@ -1,23 +1,6 @@
 # correlation plots
 library(Hmisc)
 
-# total plot
-# correlations <- rcorr(as.matrix(score_df %>%select(!c(worker_id, VisualScored,VerbalScored, high_low_verbal))))
-# cors <- correlations$r
-# ps <- cor.mtest(as.matrix(score_df%>%select(!c(worker_id, VisualScored,high_low_verbal))))$p
-# rownames(correlations$r) <- score_df_names
-# colnames(correlations$r) <- score_df_names
-# rownames(ps) <- score_df_names
-# colnames(ps) <- score_df_names
-# png(height=1800, width=1800, file="figures/cor_plot_total_v2.png")
-# total_cor <- corrplot(correlations$r,p.mat=ps, sig.level = 0.01,insig='blank', tl.cex =2, 
-#          type='lower', tl.col='black', cl.cex=2, order= 'hclust', addCoef.col = 'black')
-# dev.off()
-
-# total_corr_hclust_order <- corrMatOrder(correlations$r, order='hclust')
-# var_names <- colnames(correlations$r)
-# length(var_names[total_corr_hclust_order])
-
 # high inner speech
 score_df_high_verbal <- subset(score_df, high_low_verbal =='high_verbal')
 correlations_hv <- rcorr(as.matrix(score_df_high_verbal %>% select(!c(worker_id,VerbalScored, VisualScored,high_low_verbal))))
@@ -48,83 +31,71 @@ colnames(ps_lv) <- score_df_names
 
 # trick to plot both from: https://rpubs.com/melike/corrplot
 ord=hclust(1-as.dist(correlations_hv$r))$order
-png(height=1800, width=1800, file="../figures/cor_plot_contrast.png")
-corrplot(correlations_hv$r[ord,ord], p.mat=ps_hv[ord,ord], sig.level = 0.01,insig='blank',method='square',
+png(height=1800, width=1800, file="../figures/cor_plot_contrast_v2.png")
+corrplot(correlations_hv$r[ord,ord], p.mat=ps_hv[ord,ord],sig.level = 0.01,insig='blank',
          outline = T, addgrid.col = "white",tl.col='black',cl.pos = "r", tl.cex = 1.5, 
-         cl.cex = 1.5, mar = c(4,0,4,0), type = "upper", tl.pos = "tl", bg = '#e1deed',addCoef.col = 'white')
-corrplot(correlations_lv$r[ord,ord],p.mat=ps_lv[ord,ord], sig.level = 0.01,insig='blank',method='square',
+         cl.cex = 1.5, mar = c(4,0,4,0), type = "upper", tl.pos = "tl", bg = '#e1deed')$corrPos -> p1
+corrplot(correlations_lv$r[ord,ord],p.mat=ps_lv[ord,ord],sig.level = 0.01,insig='blank',
          outline = T, addgrid.col = "white",tl.col='black',cl.pos = "r", tl.cex = 1.5, 
-         cl.cex = 1.5, mar = c(4,0,4,0), type = "lower", tl.pos = "tl", add=T,bg = '#f2d9dd', addCoef.col = 'white')
+         cl.cex = 1.5, mar = c(4,0,4,0), type = "lower", tl.pos = "tl", add=T,bg = '#f2d9dd')$corrPos ->p2
+corrplot(correlations_hv$r[ord,ord], p.mat=ps_hv[ord,ord],sig.level = 0.01,insig='blank',method='square',
+         outline = T, addgrid.col = "white",tl.col='black',cl.pos = "r", tl.cex = 1.5, 
+         cl.cex = 1.5, mar = c(4,0,4,0), type = "upper", tl.pos = "tl", bg = '#e1deed')
+corrplot(correlations_lv$r[ord,ord],p.mat=ps_lv[ord,ord],sig.level = 0.01,insig='blank',method='square',
+         outline = T, addgrid.col = "white",tl.col='black',cl.pos = "r", tl.cex = 1.5, 
+         cl.cex = 1.5, mar = c(4,0,4,0), type = "lower", tl.pos = "tl", add=T,bg = '#f2d9dd')
+text(p1$x, p1$y, round(p1$corr, 2))
+text(p2$x, p2$y, round(p2$corr, 2))
 dev.off()
 
-# subtracted
-# cors_subtracted <- correlations_lv$r - correlations_hv$r
-# png(height=1800, width=1800, file="figures/cor_plot_subtracted_v2.png")
-# corrplot(cors_subtracted,insig='blank', tl.cex =2, 
-#          type='lower', tl.col='black', cl.cex=2, order= 'FPC')
-# dev.off()
- 
-
 # correlation plot survey
-survey_colnames <- c('Rehearse public speaking', 'Difficulty focusing on real-life conversation', 
-                     'Earworms', 'Remember friend conversation', 'Remember self conversation', 'Remembering feels like hearing',
-                     'Sing along mentally', 'Sing along feels like thinking', 'Experience of imagined voices', 
-                     'Difficulty expressing inner states', 'Inner speech sounds stressful', 'Inner speech is just a narrative tool',
-                     'Simulate future conversations' ,'Simulate past conversations', 'Impulsiveness - Do not understand own actions',
-                     'Impulsiveness - Thinking things through', 'Impulsiveness - Like acting on whim', 
-                     'Others experience conversation', "Others experience mind's eye", "Others experience mind's ear", 
+survey_colnames <- c('IRQ Verbal Score', 'ConversationFocus', 'EarWorms', 'RecallFriendConvo',
+                     'RecallSelfConvo','HearConversation',
+                     'SingAlongAbility', 'SingAlongThinking','OtherVoiceQuality',                     
+                     'ExpressInnerStates',
+                     'SimulatePastConvo', 'SimulateFutureConvo','ImpulsivenessLackOfInsight',
+                     'ImpulsivenessThinkThingsThrough', 'ImpulsivenessWhim',
+                     'OthersExperiencesConversation', "OthersExperiencesMindsEye", "OthersExperiencesMindsEar", 
                      'VISQ - Brief phrases', 'VISQ - Dialogue with self', 'VISQ - Voice of another person',
                      'VISQ - Questions from others', 'VISQ - Nagging from others', 'VISQ - Dialogue not monologue',
                      'VISQ - Full sentences', 'VISQ - Telegraphic inner speech', 'VISQ - Right or wrong actions',
                      'VISQ - Conversation with self', 'VISQ - Tell self to do things', 'VISQ - Other voices unheard utterances',
                      'VISQ - Back and forth conversation', 'VISQ - Only content words', 'VISQ - Full grammatical sentences',
                      'VISQ - Other voices already heard utterances', 'VISQ - Evaluate own behavior', 'VISQ - Tell self not to do things')
+#
+survey_df_total <- Q_anendophasia_numeric %>% select(VerbalScored, conversation_focus, earworms,
+                                                        convo_memory_friend, convo_memory_self, hearing_conversation,
+                                                        sing_along, sing_along_thinking, other_voice_quality,
+                                                        express_inner_states, rehearse_convo,
+                                                        revise_convo, impulsiveness_1, impulsiveness_2,
+                                                        impulsiveness_3, others_experiences_1, others_experiences_4,
+                                                        others_experiences_5,
+                                                        starts_with('visq'))
+# some 'other' responses should be NA
+survey_df_total$convo_memory_friend <- ifelse(survey_df_total$convo_memory_friend == 1, NA, survey_df_total$convo_memory_friend)
+survey_df_total$convo_memory_self <- ifelse(survey_df_total$convo_memory_self == 1, NA, survey_df_total$convo_memory_self)
+survey_df_total$sing_along_thinking <- ifelse(survey_df_total$sing_along_thinking == 6, NA, survey_df_total$sing_along_thinking)
 
 
-survey_df_high_verbal <- subset(Q_anendophasia_numeric, high_low_verbal == 'high_verbal')
-survey_df_high_verbal <- survey_df_high_verbal %>% select(rehearse_question, conversation_focus, earworms,
-                                                                           convo_memory_friend, convo_memory_self, hearing_conversation,
-                                                                           sing_along, sing_along_thinking, other_voice_quality,
-                                                                           express_inner_states, stress_IS, narrative_IS, rehearse_convo,
-                                                                           revise_convo, impulsiveness_1, impulsiveness_2,
-                                                                           impulsiveness_3, others_experiences_1, others_experiences_4,
-                                                                           others_experiences_5,
-                                                                           starts_with('visq'))
-correlations_hv_survey <- rcorr(as.matrix(survey_df_high_verbal))
-rownames(correlations_hv_survey$r) <- survey_colnames
-colnames(correlations_hv_survey$r) <- survey_colnames
-ps_hv_survey <- cor.mtest(as.matrix(survey_df_high_verbal))$p
-rownames(ps_hv_survey) <- survey_colnames
-colnames(ps_hv_survey) <- survey_colnames
-
-# low inner speech correlations
-survey_df_low_verbal <- subset(Q_anendophasia_numeric, high_low_verbal == 'low_verbal')
-survey_df_low_verbal <- survey_df_low_verbal %>% select(rehearse_question, conversation_focus, earworms,
-                                                                           convo_memory_friend, convo_memory_self, hearing_conversation,
-                                                                           sing_along, sing_along_thinking, other_voice_quality,
-                                                                           express_inner_states, stress_IS, narrative_IS, rehearse_convo,
-                                                                           revise_convo, impulsiveness_1, impulsiveness_2,
-                                                                           impulsiveness_3, others_experiences_1, others_experiences_4,
-                                                                           others_experiences_5,
-                                                                           starts_with('visq'))
-correlations_lv_survey <- rcorr(as.matrix(survey_df_low_verbal))
-cors_lv_survey <- correlations_lv_survey$r
-ps_lv_survey <- cor.mtest(as.matrix(survey_df_low_verbal))$p
-rownames(correlations_lv_survey$r) <- survey_colnames
-colnames(correlations_lv_survey$r) <- survey_colnames
-rownames(ps_lv_survey) <- survey_colnames
-colnames(ps_lv_survey) <- survey_colnames
+survey_df_total_cors <- rcorr(as.matrix(survey_df_total))
+cors_survey <- survey_df_total_cors$r
+ps_survey <- cor.mtest(as.matrix(survey_df_total))$p
+rownames(survey_df_total_cors$r) <- survey_colnames
+colnames(survey_df_total_cors$r) <- survey_colnames
+rownames(ps_survey) <- survey_colnames
+colnames(ps_survey) <- survey_colnames
 
 # plot both together incl correlation coefficients
 # trick to plot both from: https://rpubs.com/melike/corrplot
-ord=hclust(1-as.dist(correlations_hv_survey$r))$order
-png(height=1800, width=1800, file="figures/survey_cor_contrast.png")
-corrplot(correlations_hv_survey$r[ord,ord], p.mat=ps_hv_survey[ord,ord], sig.level = 0.01,insig='blank',
+#ord=hclust(1-as.dist(correlations_hv_survey$r))$order
+png(height=2300, width=2300, file="../figures/survey_cor_total_v2.png")
+corrplot(survey_df_total_cors$r, p.mat=ps_survey, sig.level = 0.01,insig='blank',
          outline = T, addgrid.col = "white",tl.col='black',cl.pos = "r", tl.cex = 1.5, method='square',
-         cl.cex = 1.5, mar = c(4,0,4,0), type = "upper", tl.pos = "tl", bg = '#e1deed',addCoef.col = 'white')
-corrplot(correlations_lv_survey$r[ord,ord],p.mat=ps_lv_survey[ord,ord], sig.level = 0.01,insig='blank',
-         outline = T, addgrid.col = "white",tl.col='black',cl.pos = "r", tl.cex = 1.5, method='square',
-         cl.cex = 1.5, mar = c(4,0,4,0), type = "lower", tl.pos = "tl", add=T,bg = '#f2d9dd',addCoef.col = 'white')
+         cl.cex = 1.5, mar = c(4,0,4,0), type = "lower")$corrPos -> p3
+corrplot(survey_df_total_cors$r, p.mat=ps_survey, sig.level = 0.01,insig='blank',
+         outline = T, addgrid.col = "lightgrey",tl.col='black',cl.pos = "r", tl.cex = 1.5, method='square',
+         cl.cex = 1.5, mar = c(4,0,4,0), type = "lower")
+text(p3$x, p3$y, round(p3$corr, 2))
 dev.off()
 
 # make corgram of selected questions (full participant sample)
